b063aa0ca654bb1fcd5e3c8d3d5c3cf0eeb162a1
lucontigiani <lucontigiani@gmail.com>
2021-08-01 09:55:55 -0300

    carrito de compras

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ const app= express();
 const port=3030;
 const path=require('path');
 
+app.use(express.static("public"))
+
 app.get("/",function (req, res) {
     res.sendFile(path.join(__dirname, "/views/template.html"))
 })
 
-app.use(express.static("public"))
+app.get("/",function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get('/register.html', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login.html', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/carrito', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+});
+
+app.get('/detalle.html', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/detalle.html'));
+});
 
 app.listen(port, () => 
 console.log("Levantando un servidor con Express en el puerto"+port)
