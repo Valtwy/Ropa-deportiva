89416466e7412f60f55ba907125dd8cac47f3cf5
lucontigiani <lucontigiani@gmail.com>
2021-08-17 18:41:35 -0300

    actualizacion git ignore

--- app.js
+++ app.js
@@ -16,29 +16,6 @@ app.use("/products", productsRoutes);
 
 app.use("/users", usersRoutes);
 
-
-
-
-// app.get("/",function (req, res) {
-//    res.sendFile(path.join(__dirname, "/views/home.html"))
-//})
-
-//app.get('/register', (req,res) => {
-//    res.sendFile(path.join(__dirname, '/views/register.html'));
-//});
-
-//app.get('/login', (req,res) => {
-//    res.sendFile(path.join(__dirname, '/views/login.html'));
-//});
-
-//app.get('/carrito', (req,res) => {
-//    res.sendFile(path.join(__dirname, '/views/carrito.html'));
-//});
-
-//app.get('/detalle', (req,res) => {
-//   res.sendFile(path.join(__dirname, '/views/detalle.html'));
-//});
-
 app.listen(port, () => 
 console.log("Levantando un servidor con Express en el puerto"+port)
 )
